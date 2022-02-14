4606ef2d4eacb92c973d05fc0f0628beb88b04b2
Megha Raj <megha.nambiar@gmail.com>
2022-02-14 00:30:34 -0800

    added more comments for code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ function writePassword() {
 
 /* The function generatePassword() will create the new random password based on user specified criteria.
    To do the same it will inturn call other functions to take user input , make random choices etc.
-   The code is divided into various functions for easy debugging and to promot reusability.
+   The code is divided into various functions for easy debugging and for future reusability of code.
 */
 var generatePassword = function(){
 
@@ -25,16 +25,22 @@ var generatePassword = function(){
   var password ='';
   
   var lengthInput= choosePasswordLength();
+
+  //If user chooses to cancel the password creation stop running the code.
+  //All such return statements will return empty string to prevent the UI from displaying text undefined.
   if(!lengthInput)
   {
     return "";
   }
+
+  //This function returns an array containing all character types chosen by the user.
   var characterArrayInput  = chooseCharacter();
   if(!characterArrayInput)
   {
     return "";
   }
 
+  //Provide user with a list of choices and option to confirm or cancel the process.
   var message = "Here are your choices! Press OK to generate password";
     message += "\r\n";
     message += "Password length : "+ lengthInput;
@@ -42,10 +48,12 @@ var generatePassword = function(){
     message += "Included characters : "+ characterArrayInput;
   var proceed=  window.confirm(message);
 
+  //A second copy of user choices , this will be later used to ensure that the each selected character type is chosen atleast once.
   var ensureCharacterType = [...characterArrayInput];
   
   if (proceed)
     {
+        //Run the loop untill the user's password length requirement is met.
         for (var i = 0; i < lengthInput ; i++)
         {
             var randomChoice  = pickaRandomCharacterType(characterArrayInput,ensureCharacterType);
@@ -61,20 +69,18 @@ var generatePassword = function(){
 
 /* Function provides user with the option to specify password length.
    The following validations are run to check for user input validity.
-   If any of them are not met , user is given the option to provide input again:
+   If any of them are is not met , user is given the option to provide input again:
    1. Check that input is a integer. Decimals will fail validation.
    2. Check that the input falls between 8 and 128 (inclusive).
    3. For other inputs like string, empty , decimal etc display an error message and ask user to choose again.
 
    If user presses cancel in input prompt, the program exits without generating password.
+   For this to work user input is compared with null.
 */
-
-var j =0 ;
-
 var choosePasswordLength = function(){  
-  //debugger;
   var lengthInput = window.prompt("Choose a password length between 8 and 128");
   
+  //If user presses cancel , skip execution of code.
   if(lengthInput === null)
   {
     return;
@@ -89,11 +95,13 @@ var choosePasswordLength = function(){
   }
   else{
       window.alert("Choose a number between 8 and 128. Please try again!");
+      //Recursively call the function to provide user the option to choose valid input.
       return choosePasswordLength();
   }
 }
 
 var chooseCharacter = function() {
+  //Create empty array to store user choices.
   var arrayOfChoices=[];
   var idx=0;
 
@@ -104,9 +112,11 @@ var chooseCharacter = function() {
   
   if (!lowerCaseInput && !upperCaseInput && !specialCharInput && !numberInput)
   {
+    //Using a confirm window instead of alert. This gives the user the option to cancel or proceed with password generation.
       var chooseOne= window.confirm("Please choose atleast one character type. Click OK to try again!");
       if(chooseOne)
       {
+        //Recursively call function untill user makes a choice or hits cancel.
         return chooseCharacter();
       }
       else
@@ -142,8 +152,11 @@ var chooseCharacter = function() {
 
 var pickaRandomCharacterType = function( arrayOfChoices, arrayOfChoicesOne){
   var returnVal;
+
+  //Choose a random character type each iteration.
+  // Use arrayOfChoicesOne to make sure each character type is chosen atleast once.
   if (arrayOfChoicesOne.length === 0)
-  {
+  {      
       var ch = Math.floor(Math.random()* arrayOfChoices.length);
       returnVal= arrayOfChoices[ch];
   }
@@ -156,6 +169,7 @@ var pickaRandomCharacterType = function( arrayOfChoices, arrayOfChoicesOne){
   return returnVal ;
 }
 
+//Based on a chosen character type choose a character from the global variables containing the character choices.
 var pickRandomCharacter = function(charType)
 {
     var char='';
@@ -175,7 +189,6 @@ var pickRandomCharacter = function(charType)
     }   
     return char;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
